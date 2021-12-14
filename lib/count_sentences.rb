f8e6fe8f24598bd8ccc72259b97a2b3f25afb3d4
muirisc <91299876+muirisc@users.noreply.github.com>
2021-12-13 22:05:49 -0500

    commit tree 3b0b8993401b0ff864a19508b3a2259a4ec689ea

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    
+   if self.end_with?(".")
+    true 
+  else
+    false 
   end
+end 
 
   def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
+    if self.end_with?("?")
+    true
+  else
+  false
   end
 end
+
+  def exclamation?
+   if self.end_with?("!")
+    true
+  else
+    false
+  end
+end 
+def count_sentences
+  self.split(/[.?!]/).reject(&:empty?).count
+end
+
+end 
+ 
